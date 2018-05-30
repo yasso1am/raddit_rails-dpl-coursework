af0a8c3737f4af39026b4a396e37f3943afe32b9
Andrew Yasso <andrew.yasso@gmail.com>
2018-05-30 11:58:08 -0600

    comments added

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
   resources :topics
 end
 
+  scope 'topics/:topic_id', as: 'topic' do
+    resources :comments, only: [:new, :create]
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_154705) do
+ActiveRecord::Schema.define(version: 2018_05_30_174633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "author"
+    t.bigint "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_comments_on_topic_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "name"
@@ -30,5 +39,6 @@ ActiveRecord::Schema.define(version: 2018_05_30_154705) do
     t.index ["sub_id"], name: "index_topics_on_sub_id"
   end
 
+  add_foreign_key "comments", "topics"
   add_foreign_key "topics", "subs"
 end
